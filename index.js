a0e4662effe6e77375a32a326aa690f33201fb54
jose chirinos <josechirinos11@gmail.com>
2025-12-17 12:56:18 +0100

    cosulta jeronimo n8n

--- index.js
+++ index.js
@@ -344,6 +344,52 @@ app.get("/api/pedidosComerciales", async (_, res) => {
   }
 });
 
+app.get("/api/pedidosComercialesJeronimoN8N", async (_, res) => {
+  try {
+    const rows = await connection.query(`
+      SELECT
+        [BPedidos].[Ejercicio] & '-' & [BPedidos].[Serie] & '-' & [BPedidos].[NPedido] AS NoPedido,
+        [ASecciones].[Seccion],
+        [AClientes].[NombreCliente] AS Cliente,
+        [AComerciales].[Comercial],
+        [AComerciales].[Email] AS EmailComercial,
+        [BPedidos].[RefCliente],
+        [BPedidos].[FechaCompromiso] AS Compromiso,
+        [AE].[Estado] AS EstadoPedido,
+        [BCM].[Id_ControlMat],
+        [AM].[Material],
+        [AP].[Proveedor],
+        [BCM].[FechaPrevista],
+        [BCM].[Recibido]
+      FROM
+        (((([BPedidos]
+            INNER JOIN [AClientes]
+              ON [BPedidos].[Id_Cliente] = [AClientes].[Id_Cliente])
+           INNER JOIN [AComerciales]
+              ON [AClientes].[Id_Comercial] = [AComerciales].[Id_Comercial])
+          INNER JOIN [ASecciones]
+              ON [BPedidos].[Id_Seccion] = [ASecciones].[Id_Seccion])
+         INNER JOIN [AEstadosPedido] AS [AE]
+              ON [BPedidos].[Id_EstadoPedido] = [AE].[Id_EstadoPedido])
+      LEFT JOIN
+        (
+          ([BControlMateriales] AS [BCM]
+            LEFT JOIN [AMateriales] AS [AM]
+              ON [BCM].[Id_Material] = [AM].[Id_Material])
+          LEFT JOIN [AProveedores] AS [AP]
+            ON [BCM].[Id_Proveedor] = [AP].[Id_Proveedor]
+        )
+        ON [BPedidos].[Id_Pedido] = [BCM].[Id_Pedido];
+    `);
+
+    res.json(rows);
+  } catch (err) {
+    console.error("Error al consultar Access:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 app.get("/api/pedidosComerciales40Registro", async (_, res) => {
   try {
     const rows = await connection.query(`
